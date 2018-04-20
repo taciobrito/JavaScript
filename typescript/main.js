585a182759bede6777805e44e71d997935909e7f
taciobrito <tacio.7brito@gmail.com>
2018-04-20 17:42:21 -0300

    Tipagem em TypeScript

--- typescript/main.js
+++ typescript/main.js
@@ -1,20 +1,38 @@
-function getHello(nome) {
-    return 'Hello ' + nome;
+var test = false;
+var id = 2;
+// -----------
+var decimal = 5.4;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+// -----------
+var nome = "Tácio";
+var carro = 'Camaro';
+var text = "\n\t<p>Meu Nome: " + nome + "</p>\n\t<p>Meu Carro: " + carro + "</p>\n";
+document.body.innerHTML = text;
+// -----------
+var fruits = ["Apple", "Watermelon", "Lemon"];
+console.log(fruits);
+var fruits = ["Apple", "Orange", "Lemon"];
+console.log(fruits);
+// -----------
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+console.log(Color.Red);
+console.log(Color[2]);
+console.log(Color);
+// -----------
+var listAll = 23;
+console.log(listAll);
+listAll = "String....";
+console.log(listAll);
+function printNumber(num) {
+    alert(num);
 }
-// var nome = 45;
-var nome = 'Tácio';
-document.body.innerHTML = getHello(nome);
-var Car = /** @class */ (function () {
-    function Car() {
-    }
-    return Car;
-}());
-var getCar = function (car) {
-    console.log(car.brand, car.name, car.power);
-};
-var camaro = new Car();
-camaro.brand = "Chevrolet";
-camaro.name = "Camaro";
-// camaro.age = 2016;
-camaro.power = 45;
-getCar(camaro);
+printNumber(32);
+// -----------
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,33 +1,53 @@
-function getHello(nome:string) {
-	return 'Hello ' + nome;
+var test:boolean = false;
+var id:number = 2;
+
+// -----------
+
+var decimal:number = 5.4;
+var hex:number = 0xf00d;
+var binary:number = 0b1010;
+var octal:number = 0o744;
+
+// -----------
+
+var nome:string = "Tácio";
+var carro:string = 'Camaro';
+var text:string = `
+	<p>Meu Nome: ${nome}</p>
+	<p>Meu Carro: ${carro}</p>
+`;
+
+document.body.innerHTML = text;
+
+// -----------
+
+var fruits:string[] = ["Apple", "Watermelon", "Lemon"];
+
+console.log(fruits);
+
+var fruits:Array<string> = ["Apple", "Orange", "Lemon"];
+
+console.log(fruits);
+
+// -----------
+
+enum Color {Red, Green, Blue};
+
+console.log(Color.Red);
+console.log(Color[2]);
+console.log(Color);
+
+// -----------
+
+var listAll:any = 23;
+console.log(listAll);
+listAll = "String....";
+console.log(listAll);
+
+function printNumber(num:number):void {
+	alert(num);
 }
 
-// var nome = 45;
-var nome = 'Tácio';
+printNumber(32);
 
-document.body.innerHTML = getHello(nome);
-
-interface iCar {
-	brand:string;
-	name:string;
-	power:number;
-}
-
-class Car {
-	brand:string;
-	name:string;
-	// age:number;
-	power:number;
-}
-
-var getCar = function(car:iCar){
-	console.log(car.brand, car.name, car.power);
-};
-
-var camaro = new Car();
-camaro.brand = "Chevrolet";
-camaro.name = "Camaro";
-// camaro.age = 2016;
-camaro.power = 45;
-
-getCar(camaro);
+// -----------
